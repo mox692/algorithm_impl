d195acbce11ee521fe32ef062d20baa3791165a8
mox692 <moymoymox@gmail.com>
2022-03-28 21:36:35 +0900

    wip hyper loglog

--- hyperloglog/hyperloglog_golang/hyperloglog.go
+++ hyperloglog/hyperloglog_golang/hyperloglog.go
@@ -4,25 +4,23 @@ import (
 	"math"
 )
 
-// TODO: 並列数
+// 文字列の一覧(sliceを含む)を受け取り、そのユニークな要素数の推定値を返す.
+// 上位ビットから0のビットがいくつ連続したかで推定.
 func HyperLogLog(inputData []string, hashFunc func(string) uint32) float32 {
-	// array := make([]float32, 0, len(inputData))
-	// var max float32 = 0
-	// 0000が2つ -> (2^4) * 2
 	var max float32 = 0
 	for _, v := range inputData {
 		h := hashFunc(v)
-		for pos := 32; pos >= 0; pos-- {
-			// fmt.Printf("h >> pos %032b\n", h>>pos)
-			// fmt.Printf("h        %032b\n", h)
+		for pos := 32; pos >= 1; pos-- {
 			if (h>>(pos-1))&1 == 1 {
-				if max < float32(pos) {
+				if max < float32(32-pos) {
 					max = float32(32 - pos)
 				}
 				break
 			}
 		}
 	}
+
+	// TODO: なんらかの方法で同じ入力に対して複数のhash関数を用意したい
 	ave := max / 1
 	return float32(math.Pow(2, float64(ave)))
 }
--- hyperloglog/hyperloglog_golang/hyperloglog_test.go
+++ hyperloglog/hyperloglog_golang/hyperloglog_test.go
@@ -13,36 +13,17 @@ func Test_Hyperloglog(t *testing.T) {
 		hash.Write([]byte(in))
 		s := hash.Sum(nil)
 		s = s[:4]
-
-		// 10000
 		return uint32(s[0]) | uint32(s[1])<<8 | uint32(s[2])<<16 | uint32(s[3])<<24
-		// fmt.Printf("aaaaaaaa %08b\n", s[0])
-		// max := 0
-		// for i := 0; i < len(in); i++ {
-		// 	c := 0
-		// 	for pos := 7; pos >= 0; pos-- {
-		// 		fmt.Printf("aa %08b\n", s[0]>>pos)
-		// 		if (s[0]>>pos)&1 == 1 {
-		// 			break
-		// 		}
-		// 		c++
-		// 	}
-		// 	fmt.Printf("c is %d\n", c)
-		// 	if max < c {
-		// 		max = c
-		// 	}
-		// }
-		// fmt.Printf("max is %d\n", max)
-		// return 0
 	}
 	data := [][]string{
 		{"0", "1", "a", "b", "c", "d", "e", "f", "g", "h", "i"},
+		{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"},
+		{"a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"}, // 1
+		{"b", "c", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"}, // 3
 		{"4fdsa", "xff3", "fdsai"},
-		{"4a", "f3", "ai", "fsa,", ";fa", "cfdfa", "afd", "fdsa", "fasd", "fdsafdasfdfsd"},
+		{"4a", "fdsafdasfdfsd"},
 	}
 	for _, v := range data {
 		fmt.Printf("res, %f\n", HyperLogLog(v, hashfunc))
 	}
-	// for _, v := range data {
-	// }
 }
