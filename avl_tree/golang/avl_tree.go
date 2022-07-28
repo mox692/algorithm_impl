d35310cc48b3bd51aa76687de16b96f63491865a
mox692 <moymoymox@gmail.com>
2022-07-28 19:59:01 +0900

    aaaa

--- avl_tree/golang/avl_tree.go
+++ avl_tree/golang/avl_tree.go
@@ -62,31 +62,261 @@ func NewAvlTree() *avlTree {
 }
 
 type node struct {
-	key *int
-	val string
-	l   *node
-	r   *node
+	key    *int
+	val    string
+	parent *node
+	l      *node
+	r      *node
+	lh     int
+	rh     int
 }
 
-func newNode(key *int, val string) *node {
+func newNode(parent *node, key *int, val string) *node {
 	return &node{
-		key: key,
-		val: val,
-		r:   nil,
-		l:   nil,
+		key:    key,
+		val:    val,
+		parent: parent,
+		r:      nil,
+		l:      nil,
+		lh:     0,
+		rh:     0,
 	}
 }
+
 func newNodeDefault() *node {
 	return &node{
-		r: nil,
-		l: nil,
+		key:    nil,
+		val:    "",
+		parent: nil,
+		r:      nil,
+		l:      nil,
+		lh:     0,
+		rh:     0,
 	}
+}
+
+func isBalanced(n *node) balanceType {
+	if diff := n.lh - n.rh; diff >= 0 {
+		// left heavy
+		if diff >= 2 {
+			lc := n.l
+			if lc.r == nil && lc.l != nil {
+				return UnBalancedLinearLeft
+			} else if lc.r != nil && lc.l == nil {
+				return UnBalancedCrookedLeft
+			} else {
+				log.Panicf("Invalid node, node: %+v\n", lc)
+			}
+		}
+		return balanced
+	} else {
+		// right heavy
+		if diff <= -2 {
+			rc := n.r
+			if rc.l == nil && rc.r != nil {
+				return UnBalancedLinearRight
+			} else if rc.r == nil && rc.l != nil {
+				return UnBalancedCrookedRight
+			} else {
+				log.Panicf("Invalid node, node: %+v\n", rc)
+			}
+		}
+		return balanced
+	}
+}
+
+type direction int
+
+const (
+	left direction = iota
+	right
+)
+
+type balanceType int
+
+const (
+	balanced balanceType = iota
+	UnBalancedCrookedRight
+	UnBalancedCrookedLeft
+	UnBalancedLinearRight
+	UnBalancedLinearLeft
+)
+
+// そのnodeがbalanceされているか.
+// されていなかったら、バランス処理を施してreturnする.
+// rootまで来たら(parentがなかったら)捜査は終了する
+// from -> left: 0, right:1
+func checkBalanceRec(n *node, from direction) {
+	if from == left {
+		n.lh++
+	} else if from == right {
+		n.rh++
+	}
+	parent := n.parent
+	if b := isBalanced(n); b != balanced {
+		// バランス処理.
+		// バランス結果を反映するようにする.
+		switch b {
+		case UnBalancedLinearRight:
+			if parent == nil {
+				// この時、rootはn
+				ncopy := newNode(n, n.key, n.val)
+				n.key = n.r.key
+				n.val = n.r.val
+				n.l = ncopy
+				n.r = n.r.r
+				n.parent = nil
+				ncopy.parent = n
+				n.rh = 1
+				n.lh = 1
+				n.r.rh = 0
+				n.r.lh = 0
+				n.l.rh = 0
+				n.l.lh = 0
+				return
+			}
+			ncopy := newNode(n, n.key, n.val)
+			n.key = n.r.key
+			n.val = n.r.val
+			n.l = ncopy
+			n.r = n.r.r
+			n.parent = parent
+			ncopy.parent = n
+			n.rh = 1
+			n.lh = 1
+			n.r.rh = 0
+			n.r.lh = 0
+			n.l.rh = 0
+			n.l.lh = 0
+			return
+		case UnBalancedLinearLeft:
+			if parent == nil {
+				// この時、rootはn
+				ncopy := newNode(n, n.key, n.val)
+				n.key = n.l.key
+				n.val = n.l.val
+				n.r = ncopy
+				n.l = n.l.l
+				n.parent = nil
+				ncopy.parent = n
+				n.rh = 1
+				n.lh = 1
+				n.r.rh = 0
+				n.r.lh = 0
+				n.l.rh = 0
+				n.l.lh = 0
+				return
+			}
+			ncopy := newNode(n, n.key, n.val)
+			n.key = n.l.key
+			n.val = n.l.val
+			n.r = ncopy
+			n.l = n.l.l
+			n.parent = parent
+			ncopy.parent = n
+			n.rh = 1
+			n.lh = 1
+			n.r.rh = 0
+			n.r.lh = 0
+			n.l.rh = 0
+			n.l.lh = 0
+			return
+		case UnBalancedCrookedRight:
+			//　　  1
+			//       \
+			//        3
+			//       /
+			//      2
+			nr := n.r
+			nrcopy := newNode(nr, nr.key, nr.val)
+			nrcopy.parent = nr
+			nr.key = nr.l.key
+			nr.val = nr.l.val
+			nr.r = nrcopy
+			nr.l = nil
+			//　　  1
+			//       \
+			//        2
+			//         \
+			//          3
+			ncopy := newNode(n, n.key, n.val)
+			n.key = n.r.key
+			n.val = n.r.val
+			n.l = ncopy
+			n.r = n.r.r
+			if parent == nil {
+				n.parent = nil
+			} else {
+				n.parent = parent
+			}
+			ncopy.parent = n
+			n.rh = 1
+			n.lh = 1
+			n.r.rh = 0
+			n.r.lh = 0
+			n.l.rh = 0
+			n.l.lh = 0
+		case UnBalancedCrookedLeft:
+			//　　  3
+			//    /
+			//   1
+			//    \
+			//     2
+			nl := n.l
+			nlcopy := newNode(nl, nl.key, nl.val)
+			nlcopy.parent = nl
+			nl.key = nl.r.key
+			nl.val = nl.r.val
+			nl.l = nlcopy
+			nl.r = nil
+			//　　  3
+			//    /
+			//   2
+			//  /
+			// 1
+			ncopy := newNode(n, n.key, n.val)
+			n.key = n.l.key
+			n.val = n.l.val
+			n.r = ncopy
+			n.l = n.l.l
+			n.parent = nil
+			ncopy.parent = n
+			n.rh = 1
+			n.lh = 1
+			n.r.rh = 0
+			n.r.lh = 0
+			n.l.rh = 0
+			n.l.lh = 0
+		default:
+			panic("not implement")
+		}
+	}
+	if parent == nil {
+		return
+	}
+	// そのnodeでbalanceしてるならparentをcheck
+	if parent.r != nil {
+		if parent.r.key == n.key {
+			checkBalanceRec(parent, right)
+			return
+		}
+		log.Fatal("errrrrrrrrr")
+	}
+	if parent.l != nil {
+		if parent.l.key == n.key {
+			checkBalanceRec(parent, left)
+			return
+		}
+		log.Fatal("errrrrrrrrr")
+	}
+	return
 }
 
 func setRec(n *node, key *int, val string) {
 	if *n.key < *key {
 		if n.r == nil {
-			n.r = newNode(key, val)
+			n.r = newNode(n, key, val)
+			checkBalanceRec(n, right)
 			return
 		}
 		setRec(n.r, key, val)
@@ -94,7 +324,8 @@ func setRec(n *node, key *int, val string) {
 	}
 	if *n.key > *key {
 		if n.l == nil {
-			n.l = newNode(key, val)
+			n.l = newNode(n, key, val)
+			checkBalanceRec(n, left)
 			return
 		}
 		setRec(n.l, key, val)
@@ -134,5 +365,5 @@ func flatten(n *node) string {
 			r = flatten(n.r)
 		}
 	}
-	return fmt.Sprintf("([%d:%s], (%s, %s))", *n.key, n.val, l, r)
+	return fmt.Sprintf("([%d:%s, lh: %d, rh: %d], (%s, %s))", *n.key, n.val, n.lh, n.rh, l, r)
 }
--- avl_tree/golang/avl_tree_test.go
+++ avl_tree/golang/avl_tree_test.go
@@ -1,37 +1,42 @@
 package avl_tree
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Test_Bisic(t *testing.T) {
 	tr := NewAvlTree()
-	tr.Set(1, "one")
+	tr.Set(1, "three")
 	tr.Set(2, "two")
-	tr.Set(3, "three")
-	tr.Set(4, "four")
-	tr.Set(0, "zero")
-	if got := *tr.Get(1); got != "one" {
+	tr.Set(3, "one")
+	tr.Set(4, "one")
+	tr.Set(5, "one")
+	tr.Set(6, "one")
+	// tr.Set(4, "four")
+	// tr.Set(0, "zero")
+	if got := *tr.Get(3); got != "one" {
 		t.Errorf("expect %s, got %s\n", "one", got)
 	}
 	if got := *tr.Get(2); got != "two" {
 		t.Errorf("expect %s, got %s\n", "two", got)
 	}
-	if got := *tr.Get(3); got != "three" {
+	if got := *tr.Get(1); got != "three" {
 		t.Errorf("expect %s, got %s\n", "three", got)
 	}
-	if got := *tr.Get(4); got != "four" {
-		t.Errorf("expect %s, got %s\n", "four", got)
-	}
-	if got := *tr.Get(0); got != "zero" {
-		t.Errorf("expect %s, got %s\n", "zero", got)
-	}
+	fmt.Println(flatten(tr.root))
+	// if got := *tr.Get(4); got != "four" {
+	// 	t.Errorf("expect %s, got %s\n", "four", got)
+	// }
+	// if got := *tr.Get(0); got != "zero" {
+	// 	t.Errorf("expect %s, got %s\n", "zero", got)
+	// }
 }
 
 func Test_flatten(t *testing.T) {
-	tree := NewAvlTree()
-	tree.Set(3, "af")
-	tree.Set(2, "dsfasdasdf")
-	tree.Set(6, "afdasf")
+	// tree := NewAvlTree()
+	// tree.Set(3, "af")
+	// tree.Set(2, "dsfasdasdf")
+	// tree.Set(6, "afdasf")
 	// fmt.Println(flatten(tree.root))
 }
